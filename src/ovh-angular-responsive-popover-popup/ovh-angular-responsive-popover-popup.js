fc1d08ed4aa3b9aa5366d9866af3897d67a46db1
Florian Renaut <florian.renaut@corp.ovh.com>
2018-10-19 17:33:06 +0200

    fix(popup): fix export

--- src/ovh-angular-responsive-popover-popup/ovh-angular-responsive-popover-popup.js
+++ src/ovh-angular-responsive-popover-popup/ovh-angular-responsive-popover-popup.js
@@ -1,8 +1,5 @@
 import responsivePopoverPopupDirective from './ovh-angular-responsive-popover-popup.directive';
 
 const moduleName = "responsivePopoverPopup";
-
-export default angular
-    .module("ovh-angular-responsive-popover-popup", [])
-    .directive(moduleName, responsivePopoverPopupDirective)
-    .name;
+angular.module("ovh-angular-responsive-popover-popup", []).directive(moduleName, responsivePopoverPopupDirective);
+export default moduleName;
