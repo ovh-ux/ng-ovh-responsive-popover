5729500550f954dbaaf97aa7dbbcb3895961058c
Florian Renaut <florian.renaut@corp.ovh.com>
2018-11-20 10:30:20 +0100

    build: upgrade component-rollup-config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,16 @@
-import config from '@ovh-ux/component-rollup-config';
+import rollupConfig from '@ovh-ux/component-rollup-config';
 
-export default config({
-  input: 'src/ovh-angular-responsive-popover.js',
+const config = rollupConfig({
+  input: './src/ovh-angular-responsive-popover.js',
 });
+
+export default [
+  config.cjs(),
+  config.umd({
+    output: {
+      globals: {
+        angular: 'angular',
+      },
+    },
+  }),
+];
